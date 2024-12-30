dbe164ebe3ce3226672a8ae3de1f883ea7c89186
BugHunter-X <askzenai@gmail.com>
2024-12-30 18:26:04 +0100

    Add bugSolution.ts file

---
+++ bugSolution.ts
@@ -0,0 +1,13 @@
+function greeter(person: string | string[]) {
+  if (typeof person === 'string') {
+    return "Hello, " + person;
+  } else {
+    return "Hello, " + person.join(' ');
+  }
+}
+
+let user = ["Jane", "Doe"];
+let user2 = "John";
+
+console.log(greeter(user));
+console.log(greeter(user2));
